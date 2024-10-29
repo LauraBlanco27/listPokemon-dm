7c84c3083fdf1e33cf19aabd282e6cf6f930688d
Laura Blanco <laurablancoc27@gmail.com>
2024-10-29 17:15:47 -0500

    evoluciones por name

--- src/PokemonListDm.js
+++ src/PokemonListDm.js
@@ -28,26 +28,27 @@ export class PokemonListDm extends LitElement {
   }
 
 
-  async fetchPokemonDetails(pokemonId) {
+  async fetchPokemonDetails(pokemonName) {
     try {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`);
+      // Cambia la URL para que use el nombre en lugar del ID
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`);
       const data = await response.json();
       const pokemonDetails = {
         name: data.name,
         image: data.sprites.front_default,
         types: data.types.map(typeInfo => typeInfo.type.name).join(', '),
       };
-
-      // Obtener la URL de la cadena evolutiva
-      const speciesResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonId}/`);
+  
+      // Obtener la URL de la cadena evolutiva usando la especie del Pokémon
+      const speciesResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonName}/`);
       const speciesData = await speciesResponse.json();
       const evolutionChainUrl = speciesData.evolution_chain.url;
-
+  
       // Obtener la cadena evolutiva y extraer las evoluciones con sus imágenes
       const evolutionResponse = await fetch(evolutionChainUrl);
       const evolutionData = await evolutionResponse.json();
       const evolutions = await this.extractEvolutionsWithImages(evolutionData.chain);
-
+  
       return { pokemonDetails, evolutions };
     } catch (error) {
       console.error('Error al obtener detalles del Pokémon:', error);
